e3991b1b9f51786b02990d8781246832d9abd72f
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-07-21 01:16:55 +0530

    Improved index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,27 +12,14 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 export default {
-  /**
-   * Your favorite port
-   */
    port: parseInt(process.env.PORT, 10),
 
-  /**
-   * That long string from mlab
-   */
-  databaseURL: process.env.bulletproof-template,
-  sendGridAPIKey: process.env.SENDGRID_API_KEY,
-  studioServer: process.env.STUDIO_SERVER,
+  databaseURL: process.env.CHARLIE,
   progressToken: process.env.PROGRESS_TOKEN,
-  /**
-   * Your secret sauce
-   */
+
   jwtSecret: process.env.JWT_SECRET,
   jwtAlgorithm: process.env.JWT_ALGO,
 
-  /**
-   * Used by winston logger
-   */
   logs: {
     level: process.env.LOG_LEVEL || 'silly',
   },
@@ -45,34 +32,8 @@ export default {
     epUrl: 'https://s3.us-west-1.wasabisys.com'
   },
   
-  /**
-   * Agenda.js stuff
-   */
-  // agenda: {
-  //   dbCollection: process.env.AGENDA_DB_COLLECTION,
-  //   pooltime: process.env.AGENDA_POOL_TIME,
-  //   concurrency: parseInt(process.env.AGENDA_CONCURRENCY, 10),
-  // },
-
-  /**
-   * Agendash config
-   */
-  // agendash: {
-  //   user: 'agendash',
-  //   password: '123456'
-  // },
-  /**
-   * API configs
-   */
   api: {
     prefix: '',
   },
-  /**
-   * Mailgun email credentials
-   */
-  // emails: {
-  //   apiKey: process.env.MAILGUN_API_KEY,
-  //   apiUsername: process.env.MAILGUN_USERNAME,
-  //   domain: process.env.MAILGUN_DOMAIN
-  // }
+  
 };
