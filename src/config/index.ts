5b28561b3770672714d61b6627780cf1fa3ab1d5
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-07-24 11:23:48 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   const envFound = dotenv.config();
   if (envFound.error) {
     // This error should crash whole processs
@@ -12,7 +12,7 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 export default {
-   port: parseInt(process.env.PORT, 10),
+  port: parseInt(process.env.PORT, 10),
 
   databaseURL: process.env.DB_URL,
   progressToken: process.env.PROGRESS_TOKEN,
@@ -25,15 +25,15 @@ export default {
   },
 
   aws: {
-    accessKey : process.env.AWSAccessKeyId,
-    secretKey : process.env.AWSSecretKey,
+    accessKey: process.env.AWSAccessKeyId,
+    secretKey: process.env.AWSSecretKey,
     baseBucket: 'wjsites',
     region: 'us-west-1',
     epUrl: 'https://s3.us-west-1.wasabisys.com'
   },
-  
+
   api: {
     prefix: '',
   },
-  
+
 };
